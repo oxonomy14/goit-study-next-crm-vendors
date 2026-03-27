2b087008138485d5ddb7ce14ace489e8b76a4478
oxonomy14 <oxonomy14@gmail.com>
2026-03-28 00:23:04 +0700

    added components FormModal with elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import css from './page.module.css';
 import ActiveLabel from '@/app/components/active-label';
 import NotActiveLabel from '@/app/components/not-active-label';
 import StatusLabel, { Status } from '@/app/components/status-label';
+import AddCompanyButton from '@/app/components/add-company-button';
 
 export default function Home() {
   return (
@@ -9,10 +10,11 @@ export default function Home() {
       <h1 className="text-xl">Home Page</h1>
       {/*   <ActiveLabel>Active</ActiveLabel>
       <NotActiveLabel>Not Active</NotActiveLabel> */}
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      {/*    <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel> */}
+      <AddCompanyButton />
     </main>
   );
 }
