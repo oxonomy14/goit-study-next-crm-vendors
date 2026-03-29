85728576b01689e9dcb242e1d255207dd681e06b
oxonomy14 <oxonomy14@gmail.com>
2026-03-29 22:35:07 +0700

    module_5.1: added dashboard and settings routes

---
+++ src/app/components/server-component-copy.tsx
@@ -0,0 +1,20 @@
+import { headers } from 'next/headers';
+import React from 'react';
+
+export interface ServerComponentProps {
+  children?: React.ReactNode;
+}
+
+export default function ServerComponentCopy({
+  children,
+}: ServerComponentProps) {
+  console.log('Server component Copy');
+  console.log(headers());
+
+  return (
+    <div>
+      <span>Server Component Copy</span>
+      {children}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import ActiveLabel from '@/app/components/active-label';
 import NotActiveLabel from '@/app/components/not-active-label';
 import StatusLabel, { Status } from '@/app/components/status-label';
 import AddCompanyButton from '@/app/components/add-company-button';
+import ServerComponent from '@/app/components/server-component';
+import ClientComponent from '@/app/components/client-component';
+import ServerComponentCopy from '@/app/components/server-component-copy';
 
 export default function Home() {
   return (
@@ -15,6 +18,10 @@ export default function Home() {
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
       <StatusLabel status={Status.Suspended}>Suspended</StatusLabel> */}
       <AddCompanyButton />
+      <ServerComponent />
+      <ClientComponent>
+        <ServerComponentCopy />
+      </ClientComponent>
     </main>
   );
 }
